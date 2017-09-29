9601b69081f0211a5596b6be405ea7701f03b9ae
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2017-09-29 16:03:58 -0700

    [urb-demo-webapp] Include description and links to github, other pages. CLose #440

--- units/urb-demo-webapp/components/HomePageScreen.jsx
+++ units/urb-demo-webapp/components/HomePageScreen.jsx
@@ -3,6 +3,7 @@
 import Card, { CardContent, CardHeader } from 'material-ui/Card'
 import { withStyles } from 'material-ui/styles'
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table'
+import Typography from 'material-ui/Typography'
 import PropTypes from 'prop-types'
 import React from 'react'
 import { createFragmentContainer, graphql } from 'react-relay'
@@ -47,25 +48,61 @@ class HomePageScreen extends React.Component<{
       <ResponsiveContentArea>
         <Card className={classes.card}>
           <CardHeader title="Universal Relay Boilerplate Demo" />
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Property</TableCell>
-                <TableCell numeric>Value</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map( n => {
-                return (
-                  <TableRow key={n.name}>
-                    <TableCell>{n.name}</TableCell>
-                    <TableCell numeric>{n.value}</TableCell>
-                  </TableRow>
-                )
-              })}
-            </TableBody>
-          </Table>
-          <CardContent />
+          <CardContent>
+            <Typography component="p">
+              The{' '}
+              <a href="http://codefoundries.com/products/UniversalRelayBoilerplate.html">
+                Universal Relay Boilerplate
+              </a>{' '}
+              is and open source project representing basic foundation of the solutions we provide.
+              It fully utilizes the react stack, and Node.js and Cassandra on the back end. It can
+              be used both as boilerplate, as well as an educational tool with multiple examples
+              available. Basic user account management including account creation, password strength
+              indicator and user profile is also included. The boilerplate is optimized for
+              supportability and update-ability. It allows us to update the multiple projects based
+              on the boilerplate with minimum effort, providing new features, improvements and bug
+              fixes. This is achieved through the following two approaches:
+              <ul>
+                <li>
+                  <b>Configurability</b> - All the configuration files, which include settings, CQL,
+                  JSON, snippets of JavaScript and JSX are separated from the common code.
+                </li>
+                <li>
+                  <b>Modularity</b> - The applications built upon the boilerplate are separated into
+                  semi-independent units, which contain the necessary front-end, back end, relay,
+                  CQL, etc. code. The parameters and settings for those units are stored in the
+                  configuration folder for eacy updating.
+                </li>
+              </ul>
+            </Typography>
+          </CardContent>
+        </Card>
+        <br />
+        <Card className={classes.card}>
+          <CardHeader title="Site Configuration" />
+          <CardContent>
+            <Typography component="p">
+              These settings are derived from <b>_configuration/urb-base-server/siteSettings.js</b>.
+            </Typography>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Property</TableCell>
+                  <TableCell numeric>Value</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map( n => {
+                  return (
+                    <TableRow key={n.name}>
+                      <TableCell>{n.name}</TableCell>
+                      <TableCell numeric>{n.value}</TableCell>
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </CardContent>
         </Card>
       </ResponsiveContentArea>
     )
