2d1157053a0a64854a276dbf6f2bb133ea316c8c
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2019-08-15 01:06:27 -0400

    Update to lint-staged configuration

---
+++ lint-staged.config.js
@@ -0,0 +1,55 @@
+// modeled after: https://github.com/docta/docta/blob/3569255b94789c7b205f97d225b31de7cd2c7b26/.lintstagedrc.js
+
+const read = require('fs').readFileSync
+const relative = require('path').relative
+const resolve = require('path').resolve
+
+const ignore = require('ignore')
+const micromatch = require('micromatch')
+
+// Ignores function.
+const ig = (file) => {
+  const filepath = resolve(__dirname, file)
+  const filedata = read(filepath).toString()
+  return ignore().add(filedata)
+}
+
+// Ignores
+const eslint = ig('.eslintignore')
+const prettier = ig('.prettierignore')
+
+// Filtering
+module.exports = {
+  '*': (files) => {
+    files = files.map((file) => {
+      const cmd = []
+
+      file = relative(process.cwd(), file)
+
+      //   if (
+      //     micromatch.isMatch(file, 'units/**/*.{js,jsx}', { dot: true }) &&
+      //     !eslint.ignores(file)
+      //   ) {
+      //     cmd.push(`eslint --color --fix ${file}`)
+      //   }
+
+      if (
+        micromatch.isMatch(file, 'units/**/*.{css,js,jsx,json,md,scss,yml}', {
+          dot: true
+        }) &&
+        !eslint.ignores(file) &&
+        !prettier.ignores(file)
+      ) {
+        cmd.push(`prettier-eslint --write ${file}`)
+      }
+
+      if (cmd.length > 0) {
+        cmd.push(`git add ${file}`)
+      }
+
+      return cmd.length > 0 ? cmd.join(' && ') : null
+    })
+
+    return files.filter((cmd) => cmd !== null)
+  }
+}
--- units/rb-base-tools/buildUnits.js
+++ units/rb-base-tools/buildUnits.js
@@ -15,18 +15,21 @@ import ensureFileContent from '../rb-base-server/ensureFileContent'
 
 const fs = fsWithCallbacks.promises
 
-const prettierESLintOptions = { eslintConfig: eslintRC, prettierOptions: packageJSON.prettier }
+const prettierESLintOptions = {
+  eslintConfig: eslintRC,
+  prettierOptions: packageJSON.prettier
+}
 
-function mergeScripts( scripts1, scripts2 ) {
-  const scripts = Object.assign({}, scripts1 )
+function mergeScripts(scripts1, scripts2) {
+  const scripts = Object.assign({}, scripts1)
 
-  for ( let scriptName in scripts2 ) {
+  for (let scriptName in scripts2) {
     const script = scripts[scriptName]
     const script2 = scripts2[scriptName]
 
-    if ( script && script2 ) {
+    if (script && script2) {
       scripts[scriptName] = script + ' && ' + script2
-    } else if ( script2 ) {
+    } else if (script2) {
       scripts[scriptName] = script2
     }
   }
@@ -34,20 +37,21 @@ function mergeScripts( scripts1, scripts2 ) {
   return scripts
 }
 
-async function createPackageJson( units: Array<string> ) {
-  const packageJsonFileName = path.resolve( './package.json' )
-  const currentPackageAsJSONString = ( await fs.readFile( packageJsonFileName ) ).toString()
-  const currentPackageAsObject = JSON.parse( currentPackageAsJSONString )
+async function createPackageJson(units: Array<string>) {
+  const packageJsonFileName = path.resolve('./package.json')
+  const currentPackageAsJSONString = (await fs.readFile(
+    packageJsonFileName
+  )).toString()
+  const currentPackageAsObject = JSON.parse(currentPackageAsJSONString)
   const packageAsObject = {
     dependencies: {},
     devDependencies: {},
     engines: {},
     husky: {},
-    'lint-staged': {},
     name: null,
     prettier: {},
     scripts: {},
-    version: null,
+    version: null
   }
 
   // Make sure not to overwrite version information
@@ -55,27 +59,37 @@ async function createPackageJson( units: Array<string> ) {
   packageAsObject.name = currentPackageAsObject.name
 
   // Add packages to object
-  for ( let unitName of units ) {
-    const packageAsObjectName = path.resolve( './units', unitName, 'package.part.json' )
-    if ( await fsExists( packageAsObjectName ) ) {
-      const packageToAddAsObject = JSON.parse( ( await fs.readFile( packageAsObjectName ) ).toString() )
+  for (let unitName of units) {
+    const packageAsObjectName = path.resolve(
+      './units',
+      unitName,
+      'package.part.json'
+    )
+    if (await fsExists(packageAsObjectName)) {
+      const packageToAddAsObject = JSON.parse(
+        (await fs.readFile(packageAsObjectName)).toString()
+      )
 
-      if ( packageToAddAsObject.dependencies )
-        Object.assign( packageAsObject.dependencies, packageToAddAsObject.dependencies )
-      if ( packageToAddAsObject.devDependencies )
-        Object.assign( packageAsObject.devDependencies, packageToAddAsObject.devDependencies )
-      if ( packageToAddAsObject.engines )
-        Object.assign( packageAsObject.engines, packageToAddAsObject.engines )
-      if ( packageToAddAsObject.husky )
-        Object.assign( packageAsObject.husky, packageToAddAsObject.husky )
-      if ( packageToAddAsObject['lint-staged'])
-        Object.assign( packageAsObject['lint-staged'], packageToAddAsObject['lint-staged'])
-      if ( packageToAddAsObject.prettier )
-        Object.assign( packageAsObject.prettier, packageToAddAsObject.prettier )
-      if ( packageToAddAsObject.scripts )
+      if (packageToAddAsObject.dependencies)
+        Object.assign(
+          packageAsObject.dependencies,
+          packageToAddAsObject.dependencies
+        )
+      if (packageToAddAsObject.devDependencies)
+        Object.assign(
+          packageAsObject.devDependencies,
+          packageToAddAsObject.devDependencies
+        )
+      if (packageToAddAsObject.engines)
+        Object.assign(packageAsObject.engines, packageToAddAsObject.engines)
+      if (packageToAddAsObject.husky)
+        Object.assign(packageAsObject.husky, packageToAddAsObject.husky)
+      if (packageToAddAsObject.prettier)
+        Object.assign(packageAsObject.prettier, packageToAddAsObject.prettier)
+      if (packageToAddAsObject.scripts)
         packageAsObject.scripts = mergeScripts(
           packageAsObject.scripts,
-          packageToAddAsObject.scripts,
+          packageToAddAsObject.scripts
         )
     }
   }
@@ -83,126 +97,141 @@ async function createPackageJson( units: Array<string> ) {
   await ensureFileContent(
     packageJsonFileName,
     currentPackageAsJSONString,
-    sortedJSONStringify( packageAsObject, null, 2 ),
-    true,
+    sortedJSONStringify(packageAsObject, null, 2),
+    true
   )
 }
 
-async function createMutations( units: Array<string> ) {
+async function createMutations(units: Array<string>) {
   const mutationsImports = []
   const mutationsExports = []
 
-  for ( let unitName of units )
-    if ( unitName.endsWith( '-server' ) ) {
-      const mutationsDir = path.resolve( './units', unitName, 'graphql/mutation' )
-      if ( await fsExists( mutationsDir ) ) {
-        const mutationFileNames = await fs.readdir( mutationsDir )
+  for (let unitName of units)
+    if (unitName.endsWith('-server')) {
+      const mutationsDir = path.resolve('./units', unitName, 'graphql/mutation')
+      if (await fsExists(mutationsDir)) {
+        const mutationFileNames = await fs.readdir(mutationsDir)
 
-        for ( let mutationFileName of mutationFileNames ) {
-          if ( mutationFileName.endsWith( '.js' ) ) {
-            const mutation = mutationFileName.substring( 0, mutationFileName.length - 3 )
+        for (let mutationFileName of mutationFileNames) {
+          if (mutationFileName.endsWith('.js')) {
+            const mutation = mutationFileName.substring(
+              0,
+              mutationFileName.length - 3
+            )
             mutationsImports.push(
               'import ' +
-                mutation.replace( '.', '_' ) +
+                mutation.replace('.', '_') +
                 ' from \'../../../' +
                 unitName +
                 '/graphql/mutation/' +
                 mutation +
-                '\'',
+                '\''
             )
-            mutationsExports.push( '  ' + mutation + ',' )
+            mutationsExports.push('  ' + mutation + ',')
           }
         }
       }
     }
 
-  let mutations = [ '// @flow', '' ]
-  mutations = mutations.concat( mutationsImports )
-  mutations = mutations.concat([ '', 'export default {' ])
-  mutations = mutations.concat( mutationsExports )
-  mutations = mutations.concat([ '}' ])
+  let mutations = ['// @flow', '']
+  mutations = mutations.concat(mutationsImports)
+  mutations = mutations.concat(['', 'export default {'])
+  mutations = mutations.concat(mutationsExports)
+  mutations = mutations.concat(['}'])
 
   await ensureFileContent(
-    path.resolve( './units/_configuration/rb-base-server/graphql/_mutations.js' ),
+    path.resolve('./units/_configuration/rb-base-server/graphql/_mutations.js'),
     null,
-    prettierESLint({ text: mutations.join( '\r\n' ), ...prettierESLintOptions }),
-    true,
+    prettierESLint({ text: mutations.join('\r\n'), ...prettierESLintOptions }),
+    true
   )
 }
 
-async function createSchemas( units: Array<string> ) {
+async function createSchemas(units: Array<string>) {
   const schemasImports = []
 
-  for ( let unitName of units )
-    if ( unitName.endsWith( '-server' ) ) {
-      const schemasDir = path.resolve( './units', unitName, 'graphql/model' )
-      if ( await fsExists( schemasDir ) ) {
-        const objectTypeFileNames = await fs.readdir( schemasDir )
+  for (let unitName of units)
+    if (unitName.endsWith('-server')) {
+      const schemasDir = path.resolve('./units', unitName, 'graphql/model')
+      if (await fsExists(schemasDir)) {
+        const objectTypeFileNames = await fs.readdir(schemasDir)
 
-        for ( let objectTypeFileName of objectTypeFileNames ) {
-          if ( objectTypeFileName.endsWith( '.js' ) ) {
-            const objectType = objectTypeFileName.substring( 0, objectTypeFileName.length - 3 )
+        for (let objectTypeFileName of objectTypeFileNames) {
+          if (objectTypeFileName.endsWith('.js')) {
+            const objectType = objectTypeFileName.substring(
+              0,
+              objectTypeFileName.length - 3
+            )
             schemasImports.push(
-              'import \'../../../' + unitName + '/graphql/model/' + objectType + '\'',
+              'import \'../../../' +
+                unitName +
+                '/graphql/model/' +
+                objectType +
+                '\''
             )
           }
         }
       }
     }
 
-  let schemas = [ '// @flow', '' ]
-  schemas = schemas.concat( schemasImports )
-  schemas = schemas.concat([ '', 'export default true' ])
+  let schemas = ['// @flow', '']
+  schemas = schemas.concat(schemasImports)
+  schemas = schemas.concat(['', 'export default true'])
 
   await ensureFileContent(
-    path.resolve( './units/_configuration/rb-base-server/graphql/_schemas.js' ),
+    path.resolve('./units/_configuration/rb-base-server/graphql/_schemas.js'),
     null,
-    prettierESLint({ text: schemas.join( '\r\n' ), ...prettierESLintOptions }),
-    true,
+    prettierESLint({ text: schemas.join('\r\n'), ...prettierESLintOptions }),
+    true
   )
 }
 
-async function createViewerFields( units: Array<string> ) {
+async function createViewerFields(units: Array<string>) {
   const viewerFieldsImports = []
   const viewerFieldsExports = []
 
-  for ( let unitName of units )
-    if ( unitName.endsWith( '-server' ) ) {
+  for (let unitName of units)
+    if (unitName.endsWith('-server')) {
       const viewerFieldsFileName = path.resolve(
         './units',
         unitName,
-        'graphql/type/_ViewerFields.js',
+        'graphql/type/_ViewerFields.js'
       )
-      if ( await fsExists( viewerFieldsFileName ) ) {
-        const viewerFieldsImportName = unitName.replace( /-/g, '_' )
+      if (await fsExists(viewerFieldsFileName)) {
+        const viewerFieldsImportName = unitName.replace(/-/g, '_')
         viewerFieldsImports.push(
           'import ' +
             viewerFieldsImportName +
             ' from \'../../../' +
             unitName +
-            '/graphql/type/_ViewerFields\'',
+            '/graphql/type/_ViewerFields\''
         )
-        viewerFieldsExports.push( '  ...' + viewerFieldsImportName + ',' )
+        viewerFieldsExports.push('  ...' + viewerFieldsImportName + ',')
       }
     }
 
-  let viewerFields = [ '// @flow', '' ]
-  viewerFields = viewerFields.concat( viewerFieldsImports )
-  viewerFields = viewerFields.concat([ '', 'export default {' ])
-  viewerFields = viewerFields.concat( viewerFieldsExports )
-  viewerFields = viewerFields.concat([ '}' ])
+  let viewerFields = ['// @flow', '']
+  viewerFields = viewerFields.concat(viewerFieldsImports)
+  viewerFields = viewerFields.concat(['', 'export default {'])
+  viewerFields = viewerFields.concat(viewerFieldsExports)
+  viewerFields = viewerFields.concat(['}'])
 
   await ensureFileContent(
-    path.resolve( './units/_configuration/rb-base-server/graphql/_ViewerFields.js' ),
+    path.resolve(
+      './units/_configuration/rb-base-server/graphql/_ViewerFields.js'
+    ),
     null,
-    prettierESLint({ text: viewerFields.join( '\r\n' ), ...prettierESLintOptions }),
-    true,
+    prettierESLint({
+      text: viewerFields.join('\r\n'),
+      ...prettierESLintOptions
+    }),
+    true
   )
 }
 
 async function getUnits() {
-  const units = ( await fs.readdir( './units/' ) ).filter(
-    fileName => fileName !== '.DS_Store' && fileName !== '_configuration',
+  const units = (await fs.readdir('./units/')).filter(
+    fileName => fileName !== '.DS_Store' && fileName !== '_configuration'
   )
   return units
 }
@@ -211,14 +240,14 @@ async function main() {
   const units = await getUnits()
 
   const taskPromises = [
-    createPackageJson( units ),
-    createViewerFields( units ),
-    createSchemas( units ),
-    createMutations( units ),
-    buildUnits( units ),
+    createPackageJson(units),
+    createViewerFields(units),
+    createSchemas(units),
+    createMutations(units),
+    buildUnits(units)
   ]
 
-  await Promise.all( taskPromises )
+  await Promise.all(taskPromises)
 }
 
-main().then( () => console.log( 'Fin.' ) )
+main().then(() => console.log('Fin.'))
